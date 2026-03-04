90c34cbcef7a72aff82fc81fddea41c68e9ca288
lysia123123 <947924711@qq.com>
2026-03-04 16:34:59 +0800

    feat:安装了element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css' // 引入样式文件
+import * as ElementPlusIconsVue from '@element-plus/icons-vue' // 引入图标
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// 全局注册图标 (方便以后直接用)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.use(ElementPlus)
+app.mount('#app')
